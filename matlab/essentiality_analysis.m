59c262c77950b8d2fc8047c91e79591f349a8579
idalarsson <43134255+idalarsson@users.noreply.github.com>
2020-02-18 20:41:43 +0100

    Create essentiality_analysis.m

---
+++ matlab/essentiality_analysis.m
@@ -0,0 +1,19 @@
+%Merge the 139 GBM models 
+merged_model = Cheng_mergeConditionSpecificModels(model_1, model_2);   
+for i = 3:139     
+  a=eval(['model_' num2str(i)]);     
+  merged_model = Cheng_mergeConditionSpecificModels(merged_model,a); 
+end 
+ 
+%Essentiality analysis 
+EXmodel = LTM(merged_model); 
+EXmodel = setParam(EXmodel,'obj','HCC_biomass',1); 
+EA = FastGeneSL(EXmodel_ind,0.01,1,1); 
+ 
+%Check removal of essential genes in normal brain model
+[Lia,indeces]=ismember(SingleLethalGenes,model_normal_brain.genes);     
+for i = 1:24     
+  rxnsToRemove=find(model_normal_brain.rxnGeneMat(:,indeces(i)));     
+  reducedModel=removeReactions(model_normal_brain,rxnsToRemove);              
+  [taskReport]=checkTasks(reducedModel,'common_tasks_backup.xlsx',true ,true); 
+end   
