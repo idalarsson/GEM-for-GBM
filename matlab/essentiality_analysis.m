0b0ff56a56463d9efda858450275b558c63e6bc4
idalarsson <43134255+idalarsson@users.noreply.github.com>
2020-02-20 08:48:07 +0100

    Update essentiality_analysis.m

--- matlab/essentiality_analysis.m
+++ matlab/essentiality_analysis.m
@@ -10,7 +10,7 @@ EXmodel = LTM(merged_model);
 EXmodel = setParam(EXmodel,'obj','HCC_biomass',1); 
 EA = FastGeneSL(EXmodel_ind,0.01,1,1); 
  
-%Check removal of essential genes in normal brain model
+%Check removal of essential genes (kept in vector SingleLethalGenes) in normal brain model
 [Lia,indeces]=ismember(SingleLethalGenes,model_normal_brain.genes);     
 for i = 1:24     
   rxnsToRemove=find(model_normal_brain.rxnGeneMat(:,indeces(i)));     
